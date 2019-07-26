cc38b3aa312c815b66ec03e7039fa9a22f9555fa
Arun Panchagnula <arunsai195@gmail.com>
2019-07-26 21:06:59 +1000

    Word and CSV Merge document. Word template used.

--- Test Files/6_CodeMergeWordandCSV.py
+++ Test Files/6_CodeMergeWordandCSV.py
@@ -2,7 +2,7 @@ import csv
 import sys
 from docx import Document
 
-document=Document()
+document=Document("Pillar MSSP Reporting Template.docx")
 document.add_heading('The Python code created Document')
 
 paragraph = document.add_paragraph("This is a test and first paragraph created for this project. To ensure that there is enough content in this paragraph am adding the famous sample text next to it. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
@@ -14,7 +14,6 @@ paragraph2 = document.add_paragraph('This is paragraph 2 and followed by sample 
 fields = ['Transaction_date', 'Product', 'Price', 'Payment_Type', 'Name', 'City', 'State', 'Country', 'Account_Created', 'Last_Login', 'Latitude', 'Longitude']
 
 data_table = document.add_table(rows=8, cols=2)
-data_table.style = 'LightShading-Accent1'
 
 reader = csv.DictReader(open('Sales.csv', newline=''),fieldnames=fields)
 #x=next(reader)
@@ -33,10 +32,16 @@ for row in data_table.rows:
         print(cell.text)
 
 
+#data_table.style = 'Light Shading Accent 1'
 document.add_page_break()
 
 document.add_heading('This is also a level 2 heading',level=2)
 
 paragraph3 = document.add_paragraph('This is paragraph 3 which should be followed by a line break.. So assuming this appears in a new page. The  remaining part of the paragraph is from the famous Lorum Ipsum. Mattis aliquam faucibus purus in massa. Mi bibendum neque egestas congue quisque egestas diam in. Donec adipiscing tristique risus nec feugiat in. Nunc sed id semper risus. Sed viverra tellus in hac. Consequat ac felis donec et odio pellentesque diam volutpat. Non sodales neque sodales ut etiam. Vitae elementum curabitur vitae nunc sed velit. Quisque id diam vel quam elementum pulvinar etiam. Integer vitae justo eget magna fermentum iaculis eu. Risus quis varius quam quisque id diam. Egestas maecenas pharetra convallis posuere morbi leo urna molestie.')
+document.add_page_break()
+
+#original_doc=Document("Pillar MSSP Reporting Template.docx")
+#for element in original_doc.element.body:
+#    document.element.body.append(element)
 
 document.save('CodeOutputs\My_WordDoc_CSV_Merge.docx')
