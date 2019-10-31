5c78e520c0f2dd4e0f218d6c2f2366a14cf92e23
Arun Panchagnula <arunsai195@gmail.com>
2019-11-01 10:15:29 +1100

    UI and code on 5_handlingCSV file

--- Test Files/5_handlingCSV.py
+++ Test Files/5_handlingCSV.py
@@ -1,31 +1,19 @@
-import sys
-import csv
+import sys, csv
 
 #with open('Sales.csv', newline='') as csvfile:
 fields = ['Transaction_date', 'Product', 'Price', 'Payment_Type', 'Name', 'City', 'State', 'Country', 'Account_Created', 'Last_Login', 'Latitude', 'Longitude']
 
 reader = csv.DictReader(open('Sales.csv', newline=''),fieldnames=fields)
+i=1
 x=next(reader)
-print(x['City'], x['Country'])
-x=next(reader)
-print(x['City'], x['Country'])
-x=next(reader)
-print(x['City'], x['Country'])
-x=next(reader)
-print(x['City'], x['Country'])
-x=next(reader)
-print(x['City'], x['Country'])
-x=next(reader)
-print(x['City'], x['Country'])
-x=next(reader)
-print(x['City'], x['Country'])
-x=next(reader)
-print(x['City'], x['Country'])
+print("S.No.\t", x['City'], "\t\t\t\t|", x['Country'])
+print("==============================================================")
+for x in reader:
+    print(i ,". \t", x['City'], "\t\t\t", x['Country'])
+    i+=1
+
 #   print(row)
 
-#    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')    
+#    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
 #    for row in spamreader:
 #        print(', '.join(row))
-        
-
-    
